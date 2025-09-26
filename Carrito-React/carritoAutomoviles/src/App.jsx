9c1c5e1babd5b2720d8587dd2d1d76bb55e39d0f
Nahuel C. <nahuelncejas@gmail.com>
2025-09-26 19:46:31 -0300

    Primer proyecto carrito de compra

---
+++ Carrito-React/carritoAutomoviles/src/App.jsx
@@ -0,0 +1,34 @@
+import { Automovil } from "./components/Automovil"
+import { Header } from "./components/Header"
+import {db} from "./data/db"
+
+function App() {
+
+  const data = db
+  console.log(data)
+  return (
+    <>
+      
+    <Header/>
+    <main className="container-xl mt-5">
+      <h2 className="text-center">Automóviles disponibles</h2>
+      <div className="row mt-5">
+        {data.map((automovil)=>(
+      <Automovil 
+        key={automovil.id}
+        automovil={automovil}     
+      />
+      ))}
+      </div>
+    </main>
+
+    <footer className="mt-5 py-5">
+      <div className="container-xl">
+        <p className="text-center fs-6 mt-4 m-md-0 text-muted">© 2024 Super Carros, Inc</p>
+      </div>
+    </footer>
+    </>
+  )
+}
+
+export default App
---
+++ Carrito-React/carritoAutomoviles/src/components/Automovil.jsx
@@ -0,0 +1,25 @@
+
+
+export const Automovil = ({automovil}) =>{
+
+    const { name, brand, image, year, topSpeed, price } = automovil
+
+    return(
+            <>
+                <div className="col-md-8 col-lg-4 my-4 row align-items-center mx-auto">        
+            <div className="col-10 row align-items-center mx-auto">
+            <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen automóvil" />  
+            <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+            <p>{brand}</p>
+            <p>Maxima velocidad: {topSpeed}/h</p>
+            <p>Año: {year}</p>
+            <p className="fw-black text-primary fs-3">U$D - {price}</p> 
+            <button
+                type="button"
+                className="btn btn-dark w-100"
+                >Agregar al Carrito</button>
+            </div>
+        </div>
+        </>
+    )
+}
